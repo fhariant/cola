57b38ae0cc8474c647f83dc00eb245d4ff0c67ad
Felix HARIANTO <felix.harianto@thalesgroup.com>
2021-02-24 15:53:45 +0800

    https

--- amplify/backend/function/hai/src/main/java/example/LambdaRequestHandler.java
+++ amplify/backend/function/hai/src/main/java/example/LambdaRequestHandler.java
@@ -12,7 +12,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.HttpsURLConnection;
+import javax.net.ssl.HttpsURLConnection;
 import java.net.URL;
 
 public class LambdaRequestHandler implements RequestHandler<RequestClass, ResponseClass>{   
