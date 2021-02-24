5c755297001f733c997e01a416e779eb73ab3ca8
Felix HARIANTO <felix.harianto@thalesgroup.com>
2021-02-24 15:59:40 +0800

    fixx

--- amplify/backend/function/hai/src/main/java/example/LambdaRequestHandler.java
+++ amplify/backend/function/hai/src/main/java/example/LambdaRequestHandler.java
@@ -12,6 +12,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import javax.net.ssl.HttpsURLConnection;
 import java.net.URL;
 
