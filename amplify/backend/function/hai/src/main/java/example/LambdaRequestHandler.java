c336dc2a66b1a16d6f70fc811d5c9178730de746
Felix HARIANTO <felix.harianto@thalesgroup.com>
2021-02-24 14:26:46 +0800

    sendGET()

--- amplify/backend/function/hai/src/main/java/example/LambdaRequestHandler.java
+++ amplify/backend/function/hai/src/main/java/example/LambdaRequestHandler.java
@@ -8,6 +8,13 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class LambdaRequestHandler implements RequestHandler<RequestClass, ResponseClass>{   
 
     public ResponseClass handleRequest(RequestClass request, Context context){
@@ -29,6 +36,34 @@ public class LambdaRequestHandler implements RequestHandler<RequestClass, Respon
             greetingString = e.getMessage();
         }
 		
+		sendGET();
+		
         return new ResponseClass(greetingString);
     }
+	
+	private static void sendGET() throws IOException {
+		URL obj = new URL("https://www.google.com");
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		int responseCode = con.getResponseCode();
+		System.out.println("GET Response Code :: " + responseCode);
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			System.out.println(response.toString());
+		} else {
+			System.out.println("GET request not worked");
+		}
+
+	}
 }
